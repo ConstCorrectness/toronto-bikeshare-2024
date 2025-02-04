9d3ea5942e623f49b82fd70988e2b2ff414f4198
horriblecpp <horribleccprogram@proton.me>
2025-02-03 21:16:31 -0500

    skeleton

--- src/torontobikeshare/pulljson.py
+++ src/torontobikeshare/pulljson.py
@@ -1,8 +1,16 @@
 import json
 import requests
-
+import pandas as pd
+import csv
 
 station_information = 'https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information'
 station_status = 'https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_status'
 
 
+response = requests.get(station_information)
+if 'json' in response.headers['Content-Type']:
+  json_data = response.json()  
+  stations_list = json_data['data']['stations']
+  print(stations_list[0])
+  
+  
