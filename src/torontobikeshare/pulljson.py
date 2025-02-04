528b2c1182e3c379d1486b862296c501d71efd76
horriblecpp <horribleccprogram@proton.me>
2025-02-03 21:04:58 -0500

    requirements.txt

---
+++ src/torontobikeshare/pulljson.py
@@ -0,0 +1,8 @@
+import json
+import requests
+
+
+station_information = 'https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_information'
+station_status = 'https://tor.publicbikesystem.net/ube/gbfs/v1/en/station_status'
+
+
