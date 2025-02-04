3b7b7fc135655823e634d76819e45fe2c1d64104
horriblecpp <horribleccprogram@proton.me>
2025-02-04 17:59:21 -0500

    google static maps API code

--- src/torontobikeshare/main.py
+++ src/torontobikeshare/main.py
@@ -1,3 +1,10 @@
-def main():
-  print('hello')
-  
+import googlemaps
+import googlemaps.maps as maps
+
+import config
+import pandas as pd
+
+
+
+gmaps = googlemaps.Client(config.google_maps_api_key)
+
